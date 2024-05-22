47c83c09fd4dd02b52923d0b83f059fd19ab42fb
Fer Baeza <fbaezahurtado@gmail.com>
2024-05-22 09:08:50 +0000

    composer.json

---
+++ src/Laravel/Console/Example/ExampleCommandHandler.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Baezeta\Kernel\Laravel\Console\Example;
+
+class ExampleCommandHandler
+{
+
+    public function run(ExampleCommand $command)
+    {
+        return true;
+    }
+}
